16516ed5a30b5e70760f714a9a1c8a6b4cee6cac
Marco Aguero <marcoferab@gmail.com>
2026-05-23 22:06:36 -0600

    fix: nav links

--- src/components/globals/header.tsx
+++ src/components/globals/header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import { usePathname } from 'next/navigation'
 import { AnimatePresence, motion } from 'motion/react'
 import { Menu, X } from 'lucide-react'
